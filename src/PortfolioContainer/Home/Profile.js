6c7f496dd5a94693ab20b819c2ccd5f3c34c3974
AlvaroPerezRey <aperez.alumno@iesrafaelalberti.es>
2022-02-02 19:48:39 +0100

    Creado componente Profile

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,74 @@
+import React from "react";
+import Typical from "react-typical";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="https://github.com/AlvaroPerezRey">
+                <i className="fa fa-github-square"></i>
+              </a>
+              <a href="https://www.instagram.com/">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="https://www.facebook.com/">
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a href="https://twitter.com/">
+                <i className="fa fa-twitter-square"></i>
+              </a>
+              <a href="https://www.youtube.com/">
+                <i className="fa fa-youtube-square"></i>
+              </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'm <span className="highlighted-text">Alvaro</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Ethusiastic Dev 🔥",
+                    1000,
+                    "Full Stack Developer ⚡️",
+                    1000,
+                    "Mern Stack Dev 🎉",
+                    1000,
+                    "Cross Platform 👨‍💻",
+                    1000,
+                    "React/React Native Dev 💻",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Knack of building applications with fron and back end
+                operations.
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn"> Hire me </button>
+            <a href="CV_Alvaro.pdf" download="Alvaro CV_Alvaro.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
