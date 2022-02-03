3fcfe91a9d6e3a8fe168d1f22e7e9ee2c5df6efd
AlvaroPerezRey <aperez.alumno@iesrafaelalberti.es>
2022-02-03 14:04:04 +0100

    Responsive y Header hechos, comenzando About

---
+++ src/PortfolioContainer/About/About.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react';
+import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
+import ScrollService from '../../utilities/ScrollService';
+import Animations from '../../utilities/Animations';
+
+export default function About() {
+  return (
+  <div>
+
+  </div>
+  )
+}
