2c12b6aa71df28f372460a792c67ee7af5ecba68
AlvaroPerezRey <aperez.alumno@iesrafaelalberti.es>
2022-02-08 19:58:01 +0100

    Componente Resume terminado con estilos

--- src/PortfolioContainer/Resume/Resume.js
+++ src/PortfolioContainer/Resume/Resume.js
@@ -1,20 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollService";
 import Animations from "../../utilities/Animations";
+import "./Resume.css";
 
-export default function Resume(props) {
+const Resume = (props) => {
+  /* STATES */
   const [selectedBulletIndex, setSelectedBulletIndex] = useState(0);
-  const [carouselOffSetStyle, setCarouselOffSetStyle] = useState({});
+  const [carousalOffsetStyle, setCarousalOffsetStyle] = useState({});
 
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  /* REUSABLE MINOR COMPONENTS */
   const ResumeHeading = (props) => {
-    <div className="resume-heading">
-      <div className="resume-main-heading">
-        <div className="heading-bullet">
+    return (
+      <div className="resume-heading">
+        <div className="resume-main-heading">
+          <div className="heading-bullet"></div>
           <span>{props.heading ? props.heading : ""}</span>
           {props.fromDate && props.toDate ? (
             <div className="heading-date">
-              {props.fromDate + "_" + props.toDate}
+              {props.fromDate + "-" + props.toDate}
             </div>
           ) : (
             <div></div>
@@ -27,9 +39,10 @@ export default function Resume(props) {
           <span>{props.description ? props.description : ""}</span>
         </div>
       </div>
-    </div>;
+    );
   };
 
+  /* STATIC RESUME DATA FOR THE LABELS*/
   const resumeBullets = [
     { label: "Education", logoSrc: "education.svg" },
     { label: "Work History", logoSrc: "work-history.svg" },
@@ -38,151 +51,217 @@ export default function Resume(props) {
     { label: "Interests", logoSrc: "interests.svg" },
   ];
 
-  const programmingSkillDetails = [
+  //here we have
+  const programmingSkillsDetails = [
     { skill: "JavaScript", ratingPercentage: 85 },
-    { skill: "JavaScript", ratingPercentage: 85 },
-    { skill: "JavaScript", ratingPercentage: 85 },
-    { skill: "JavaScript", ratingPercentage: 85 },
-    { skill: "JavaScript", ratingPercentage: 85 },
-    { skill: "JavaScript", ratingPercentage: 85 },
-    { skill: "JavaScript", ratingPercentage: 85 },
-    { skill: "JavaScript", ratingPercentage: 85 },
+    { skill: "React JS", ratingPercentage: 85 },
+    { skill: "React Native", ratingPercentage: 85 },
+    { skill: "Express JS", ratingPercentage: 89 },
+    { skill: "Node JS", ratingPercentage: 89 },
+    { skill: "Mongo Db", ratingPercentage: 70 },
+    { skill: "Core Java", ratingPercentage: 80 },
+    { skill: "HTML", ratingPercentage: 80 },
+    { skill: "CSS", ratingPercentage: 80 },
   ];
 
-  const projectDetails = [
+  const projectsDetails = [
     {
       title: "Personal Portfolio Website",
-      duration: { fromDate: "2022", toDate: "2022" },
-      description: "A Personal Portfolio website to Showcase all my details",
-      subHeading: "Technologies Used: ReactJS, Bootstrap",
+      duration: { fromDate: "2020", toDate: "2021" },
+      description:
+        "A Personal Portfolio website to showcase all my details and projects at one place.",
+      subHeading: "Technologies Used: React JS, Bootsrap",
     },
     {
-      title: "Personal Portfolio Website",
-      duration: { fromDate: "2022", toDate: "2022" },
-      description: "A Personal Portfolio website to Showcase all my details",
-      subHeading: "Technologies Used: ReactJS, Bootstrap",
+      title: "Mobile E-shop ",
+      duration: { fromDate: "2020", toDate: "2021" },
+      description:
+        "An ecommerce application designed to sell products online wth payment system integration",
+      subHeading:
+        "Technologies Used:  React Native, Mongo DB, Express Js, Node Js, Redux.",
     },
     {
-      title: "Personal Portfolio Website",
-      duration: { fromDate: "2022", toDate: "2022" },
-      description: "A Personal Portfolio website to Showcase all my details",
-      subHeading: "Technologies Used: ReactJS, Bootstrap",
+      title: "Ecommerce Website ",
+      duration: { fromDate: "2020", toDate: "2021" },
+      description:
+        "Online ecommerce website for showcasing and selling products onlne with payment system integration, both Paypal and Stripe",
+      subHeading:
+        "Technologies Used: Mongo DB, Epress Js, React Js, Node JS, Redux, Bootstrap.",
     },
   ];
 
   const resumeDetails = [
-    <div className="resume.screen-container" key="education">
+    <div className="resume-screen-container" key="education">
       <ResumeHeading
-        heading={"University of Legon Accra, India"}
-        subHeading={"Bachelor of Science information Technology"}
-        fromDate={"2020"}
-        toDate={"2021"}
+        heading={"University of Legon Accra, Ghana"}
+        subHeading={"BACHELOR OF SCIENCE INFORMATION TECHNOLOGY"}
+        fromDate={"2014"}
+        toDate={"2018"}
+      />
+
+      <ResumeHeading
+        heading={"National Youth Service Corps"}
+        subHeading={"Ministry Of Science And Technogy. Uyo Akwa Ibom State"}
+        fromDate={"2019"}
+        toDate={"2020"}
       />
       <ResumeHeading
-        heading={"University of Legon Accra, India"}
-        subHeading={"Bachelor of Science information Technology"}
-        fromDate={"2020"}
-        toDate={"2021"}
-      />
-      <ResumeHeading
-        heading={"University of Legon Accra, India"}
-        subHeading={"Bachelor of Science information Technology"}
-        fromDate={"2020"}
-        toDate={"2021"}
-      />
-      <ResumeHeading
-        heading={"University of Legon Accra, India"}
-        subHeading={"Bachelor of Science information Technology"}
-        fromDate={"2020"}
-        toDate={"2021"}
+        heading={"High School "}
+        subHeading={"Command Secondary School Mbiri"}
+        fromDate={"2007"}
+        toDate={"2012"}
       />
     </div>,
-    <div className="resume.screen-container" key="work-experience">
-      <ResumeHeading
-        heading={"Alvaro Technology"}
-        subHeading={"Full Stack hermanito"}
-        fromDate={"2018"}
-        toDate={"Present"}
-      />
-      <div className="experience-description">
-        <span className="resume-description-text">
-          Currently working as Student in Caisity ya tu sabe pa
-        </span>
-      </div>
-      <div className="experience-description">
-        <span className="resume-description-text">
-          Developeando un poquito de cada sitio que veo
-        </span>
-        <br />
-        <span className="resume-description-text">
-          Developeando un poquito de cada sitio que veo
-        </span>
-        <br />
-        <span className="resume-description-text">
-          Developeando un poquito de cada sitio que veo
-        </span>
-      </div>
-      ,
-      <div
-        className="resume-screen-container programming-skills-container"
-        key="programming-skills"
-      >
-        {programmingSkillDetails.map((skill, index) => (
-          <div className="skill-parent" key={index}>
-            <div className="heading-bullet"></div>
-            <span>{skill.skill}</span>
-            <div className="skill-percentage">
-              <div
-                style={{ width: skill.ratingPercentage + "%" }}
-                className="active-percentage"
-              ></div>
-            </div>
-          </div>
-        ))}
-      </div>,
 
-        <div className="resume-screen-container" key="projects">
-            {projectDetails.map((projectsDetails, index) => (
-                <ResumeHeading
-                key={index}
-                heading={projectsDetails.title}
-                subHeading={projectsDetails.subHeading}
-                description={projectsDetails.description}
-                fromDate={projectsDetails.duration.fromDate}
-                toDate={projectsDetails.duration.toDate}
-                />
-            ))}
-        </div>,
-        <div className="resume-screen-container" key="interests">
-            <ResumeHeading
-            heading="Learning"
-            description="Apart from being a tech enthusiast and a code writer"
-            />
-                        <ResumeHeading
-            heading="Learning"
-            description="Apart from being a tech enthusiast and a code writer"
-            />
-                        <ResumeHeading
-            heading="Learning"
-            description="Apart from being a tech enthusiast and a code writer"
-            />
+    /* WORK EXPERIENCE */
+    <div className="resume-screen-container" key="work-experience">
+      <div className="experience-container">
+        <ResumeHeading
+          heading={"Ehizeex Technoloy"}
+          subHeading={"FULL STACK DEVELOPER INTERN"}
+          fromDate={"2021"}
+          toDate={"Present"}
+        />
+        <div className="experience-description">
+          <span className="resume-description-text">
+            Currently working as MERN stack web and mobile developer and also an
+            online instructor on udemy.
+          </span>
         </div>
+        <div className="experience-description">
+          <span className="resume-description-text">
+            - Developed an ecommerce website for client with the dashboard for
+            managing the products, managing reviews, users, payment etc. .
+          </span>
+          <br />
+          <span className="resume-description-text">
+            - Integrated the web app with backend services to create new user
+            onboarding application with dynamic form content.{" "}
+          </span>
+          <br />
+          <span className="resume-description-text">
+            - I stretch my mental capacity to develope UI as per the given
+            designs.
+          </span>
+          <br />
+        </div>
+      </div>
+    </div>,
 
-    </div>
+    /* PROGRAMMING SKILLS */
+    <div
+      className="resume-screen-container programming-skills-container"
+      key="programming-skills"
+    >
+      {programmingSkillsDetails.map((skill, index) => (
+        <div className="skill-parent" key={index}>
+          <div className="heading-bullet"></div>
+          <span>{skill.skill}</span>
+          <div className="skill-percentage">
+            <div
+              style={{ width: skill.ratingPercentage + "%" }}
+              className="active-percentage-bar"
+            ></div>
+          </div>
+        </div>
+      ))}
+    </div>,
+
+    /* PROJECTS */
+    <div className="resume-screen-container" key="projects">
+      {projectsDetails.map((projectsDetails, index) => (
+        <ResumeHeading
+          key={index}
+          heading={projectsDetails.title}
+          subHeading={projectsDetails.subHeading}
+          description={projectsDetails.description}
+          fromDate={projectsDetails.duration.fromDate}
+          toDate={projectsDetails.duration.toDate}
+        />
+      ))}
+    </div>,
+
+    /* Interests */
+    <div className="resume-screen-container" key="interests">
+      <ResumeHeading
+        heading="Teaching"
+        description="Apart from being a tech enthusiast and a code writer, i also love to teach people what i know simply because i believe in sharing."
+      />
+      <ResumeHeading
+        heading="Music"
+        description="Listening to soothing music is something i can never compromise with, skimming through Spotify's pop songs charts is at times the best stress reliever that i can get my hands on."
+      />
+      <ResumeHeading
+        heading="Competitive Gaming"
+        description="I like to challenge my reflexes a lot while competing in football games, pushing the rank and having interactive gaming sessions excites me the most."
+      />
+    </div>,
   ];
 
-  let fadeInScreenHandler = (screen) => {
-    if (screen.fadeScreen !== props.id) return;
-    Animations.animations.fadeInScreen(props.id);
+  const handleCarousal = (index) => {
+    let offsetHeight = 360;
+
+    let newCarousalOffset = {
+      style: { transform: "translateY(" + index * offsetHeight * -1 + "px)" },
+    };
+
+    setCarousalOffsetStyle(newCarousalOffset);
+    setSelectedBulletIndex(index);
   };
-  const fadeInSubscription =
-    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const getBullets = () => {
+    return resumeBullets.map((bullet, index) => (
+      <div
+        onClick={() => handleCarousal(index)}
+        className={
+          index === selectedBulletIndex ? "bullet selected-bullet" : "bullet"
+        }
+        key={index}
+      >
+        <img
+          className="bullet-logo"
+          src={require(`../../assets/Resume/${bullet.logoSrc}`)}
+        />
+        <span className="bullet-label">{bullet.label}</span>
+      </div>
+    ));
+  };
+
+  const getResumeScreens = () => {
+    return (
+      <div
+        style={carousalOffsetStyle.style}
+        className="resume-details-carousal"
+      >
+        {resumeDetails.map((ResumeDetail) => ResumeDetail)}
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    return () => {
+      /* UNSUBSCRIBE THE SUBSCRIPTIONS */
+      fadeInSubscription.unsubscribe();
+    };
+  }, [fadeInSubscription]);
 
   return (
-    <div className="resume-container screen-container" id={props.id || ""}>
+    <div className="resume-container screen-container " id={props.id || ""}>
       <div className="resume-content">
-        <ScreenHeading title={"Resume"} subHeading={"My Formal Bio Details"} />
+        <ScreenHeading title={"Resume"} subHeading={"My formal Bio Details"} />
+        <div className="resume-card">
+          <div className="resume-bullets">
+            <div className="bullet-container">
+              <div className="bullet-icons"></div>
+              <div className="bullets">{getBullets()}</div>
+            </div>
+          </div>
+
+          <div className="resume-bullet-details">{getResumeScreens()}</div>
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default Resume;
